7e09d6bc99e4169b0742581e78e00b7d34508c61
Emmanuel Arias Soto <ariassotoemmanuel@gmail.com>
2026-01-16 19:43:16 +0100

    Integrate landmark drawing code into MP service

--- Model/Test/MediaPipeService.py
+++ Model/Test/MediaPipeService.py
@@ -1,6 +1,5 @@
 import cv2
 import mediapipe as mp
-from LandmarkDrawer import LandmarkDrawer
 from Utilities import Utilities
 
 
@@ -13,7 +12,6 @@ class MediaPipeService:
     def __init__(self):
         self.mp_holistic = mp.solutions.holistic
         self.mp_drawing = mp.solutions.drawing_utils
-        self.drawer = LandmarkDrawer(self.mp_drawing, self.mp_holistic)
         self.logger = Utilities.setup_logging()
 
     def mediapipe_detection(self, frame, model):
@@ -26,4 +24,20 @@ class MediaPipeService:
 
     def draw_landmarks(self, frame, results):
         """Draws detected landmarks on the frame."""
-        self.drawer.draw(frame, results)
+        if results.face_landmarks:
+            self.mp_drawing.draw_landmarks(
+                frame,
+                results.face_landmarks,
+                self.mp_holistic.FACEMESH_TESSELATION,
+                self.mp_drawing.DrawingSpec(color=(103, 207, 245), thickness=1, circle_radius=1),
+                self.mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=1, circle_radius=1),
+            )
+
+        if results.pose_landmarks:
+            self.mp_drawing.draw_landmarks(frame, results.pose_landmarks, self.mp_holistic.POSE_CONNECTIONS)
+
+        if results.left_hand_landmarks:
+            self.mp_drawing.draw_landmarks(frame, results.left_hand_landmarks, self.mp_holistic.HAND_CONNECTIONS)
+
+        if results.right_hand_landmarks:
+            self.mp_drawing.draw_landmarks(frame, results.right_hand_landmarks, self.mp_holistic.HAND_CONNECTIONS)
